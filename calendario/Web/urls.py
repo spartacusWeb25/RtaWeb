ac24b2966f16538e48c26a3215d3c43c6c3127e4
leonardo araujo <leokaique7@gmail.comm>
2026-05-03 17:27:42 -0300

    ok

--- calendario/Web/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from calendario.web.views import CalendarioEventoCreateView, CalendarioEventosJsonView
-
-urlpatterns = [
-    path("calendario/evento/criar/", CalendarioEventoCreateView.as_view(), name="evento_criar"),
-    path("calendario/eventos/json/", CalendarioEventosJsonView.as_view(), name="eventos_json"),
-]
